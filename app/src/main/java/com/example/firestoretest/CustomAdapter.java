2608c624e0ae99d0a9e49842e4c73e99946594b9
Yongchan <124363127+parteylongpass@users.noreply.github.com>
2023-11-22 13:30:09 +0900

    11-22

---
+++ app/src/main/java/com/example/firestoretest/CustomAdapter.java
@@ -0,0 +1,73 @@
+package com.example.firestoretest;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder>{
+    //필드
+    private ArrayList<Stuff> localDataSet;
+    //생성자
+    public CustomAdapter(ArrayList<Stuff> localDataSet) {
+        this.localDataSet = localDataSet;
+    }
+
+    //어댑터 오버라이드 함수
+    //뷰홀더 객체 생성과 초기화
+    @NonNull
+    @Override
+    public CustomAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    //데이터를 가져와 뷰홀더 안의 내용을 채움
+    @Override
+    public void onBindViewHolder(@NonNull CustomAdapter.ViewHolder holder, int position) {
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        MainActivity mainActivity = new MainActivity();
+        holder.name.setText(localDataSet.get(position).getName());
+        holder.quantity.setText(String.valueOf(localDataSet.get(position).getQuantity()));
+        //profile은 어떻게 하지...
+    }
+
+    //총 아이템의 개수 반환
+    @Override
+    public int getItemCount() {
+        return localDataSet.size();
+    }
+
+    //뷰홀더 클래스 정의
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        //item.xml에 만든 '아이템 틀'의 요소들을 불러들어옴
+        private ImageView profile;
+        private TextView name;
+        private TextView quantity;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            profile = itemView.findViewById(R.id.profile_iv);
+            name = itemView.findViewById(R.id.stuff_name_tv);
+            quantity = itemView.findViewById(R.id.quantity_tv);
+        }
+    }
+
+    //검색 기능 구현
+    public void setItems(ArrayList<Stuff> list){
+        localDataSet = list;
+        notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/com/example/firestoretest/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.firestoretest;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<Stuff> dataSet;
+
+    //검색 기능 구현
+    EditText searchET;
+    ArrayList<Stuff> filteredList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("YYC", "onCreate");
+        setContentView(R.layout.activity_main);
+
+        dataSet = new ArrayList<>();
+
+        //activity_main.xml에 만든 RecyclerView와 연결
+        RecyclerView recyclerView = findViewById(R.id.recycler_view_rv);
+
+        //LayoutManager 객체 생성 후 RecylcerView 객체와 연결
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //CustomAdapter 객체 생성 후 RecyclerView 객체와 연결
+        CustomAdapter customAdapter = new CustomAdapter(dataSet);
+        recyclerView.setAdapter(customAdapter);
+
+        //Firestore 이용
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("stuff").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    Log.d("TAG", "Listen failed.");
+                    return;
+                }
+
+                if(value != null) {
+                    dataSet.clear();
+                    for (DocumentSnapshot snapshot : value.getDocuments()) {
+                        Stuff stuff = snapshot.toObject(Stuff.class);
+                        if (stuff != null) {
+                            dataSet.add(stuff);
+                        }
+                    }
+                    customAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+
+        //검색 기능 구현
+        searchET = findViewById(R.id.search_tv);
+        filteredList = new ArrayList<>();
+
+        searchET.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String searchText = searchET.getText().toString();
+                filteredList.clear();
+
+                if(searchText.equals("")){
+                    customAdapter.setItems(dataSet);
+                }
+                else {
+                    // 검색 단어를 포함하는지 확인
+                    for (int i = 0; i < dataSet.size(); i++) {
+                        if (dataSet.get(i).getName().toLowerCase().contains(searchText.toLowerCase())) {
+                            filteredList.add(dataSet.get(i));
+                        }
+                        customAdapter.setItems(filteredList);
+                    }
+                }
+            }
+        });
+
+    }
+}
